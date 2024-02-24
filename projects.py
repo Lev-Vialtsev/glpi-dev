4dea7b7e91f5c06546ae6730059459e032576eb0
Вяльцев Лев <levanatolievich08@gmail.com>
2024-02-24 23:08:19 +1000

    Не полуичлось сделать ООП тест коммита

--- projects.py
+++ projects.py
@@ -6,143 +6,161 @@ from telebot import types
 
 bot_token = '6964646666:AAG1gwMBWXE439pydC54btwBEkuqPK93dDU'
 bot = telebot.TeleBot(bot_token)
+uname_upass = []
+search_id = 0
+flag = False
 
-class User:
-    def __init__(self, user_id):
-        self.user_id = user_id
-        self.is_registered = False
-        self.name = None
-        self.password = None
 
-    def is_user_registered(self):
-        conn = sqlite3.connect('database_reg.db')
-        cur = conn.cursor()
-        cur.execute('SELECT * FROM reg_inf WHERE usid = ?', (self.user_id,))
-        result = cur.fetchone()
-        cur.close()
-        conn.close()
-        if result:
-            self.is_registered = True
-            self.name = result[1]
-            self.password = result[2]
-        return self.is_registered
+@bot.message_handler(commands=['start'])
+def start(message):
+    user_id = message.from_user.id
+    # Проверяем, есть ли id пользователя в базе данных
+    if is_user_registered(message, user_id):
+        # Пользователь уже зарегистрирован
+        markup = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+        button_text = "Создать заявку"
+        button = types.KeyboardButton(text=button_text)
+        markup.add(button)
+        bot.send_message(message.chat.id, "Выберите действие:", reply_markup=markup)
+    else:
+        # Пользователь не зарегистрирован
+        bot.send_message(message.chat.id, "Для начала вам нужно зарегистрироваться. Введите '/register'.")
 
-    def register_user(self, name, password):
-        conn = sqlite3.connect('database_reg.db')
-        cur = conn.cursor()
-        cur.execute('INSERT INTO reg_inf(name, pass, usid) VALUES(?, ?, ?)', (name, password, self.user_id))
-        conn.commit()
-        cur.close()
-        conn.close()
-        self.is_registered = True
-        self.name = name
-        self.password = password
-
-    def unregister_user(self):
-        conn = sqlite3.connect('database_reg.db')
-        cur = conn.cursor()
-        cur.execute('DELETE from reg_inf WHERE name = ? AND pass = ? AND usid = ?', (self.name, self.password, self.user_id))
-        conn.commit()
-        cur.close()
-        conn.close()
-        self.is_registered = False
-        self.name = None
-        self.password = None
-
-class Bot:
-    def __init__(self, bot_token):
-        self.bot = telebot.TeleBot(bot_token)
-        self.users = {}
+def is_user_registered(message, user_id):
+    global search_id
+    conn = sqlite3.connect('D:\Desktop\Yandex_tasks\GLPItelegramBot\database_reg.db')
+    cur = conn.cursor()
+    
+    cur.execute('SELECT * FROM reg_inf WHERE usid = ?', (message.from_user.id,))
+    search_id = cur.fetchone()
+    
+    cur.close()  
+    conn.close()
+    if search_id:
+        return True
+    else:
+        return False
+    
+@bot.message_handler(commands=['register'])
+def register_user(message):
+    global search_id
+    conn = sqlite3.connect('D:\Desktop\Yandex_tasks\GLPItelegramBot\database_reg.db')
+    cur = conn.cursor()
+    cur.execute('''CREATE TABLE IF NOT EXISTS
+    reg_inf(id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    name TEXT, 
+    pass TEXT,
+    usid INTEGER
+    )''')
+    conn.commit()
+    
+    cur.execute('SELECT * FROM reg_inf WHERE usid = ?', (message.from_user.id,))
+    search_id = cur.fetchone()
+    
+    if search_id:
+        bot.send_message(message.chat.id, 'Вы уже зарегистрированы!')
         
-    @bot.message_handler(commands=['start'])
-    def start(self, message):
-        user_id = message.from_user.id
-        if user_id not in self.users:
-            self.users[user_id] = User(user_id)
-        user = self.users[user_id]
-        if user.is_user_registered():
-            markup = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-            button_text = "Создать заявку"
-            button = types.KeyboardButton(text=button_text)
-            markup.add(button)
-            self.bot.send_message(message.chat.id, "Выберите действие:", reply_markup=markup)
-        else:
-            self.bot.send_message(message.chat.id, "Для начала вам нужно зарегистрироваться. Введите '/register'.")
-
-    @bot.message_handler(commands=['register'])
-    def register_user(self, message):
-        user_id = message.from_user.id
-        user = self.users[user_id]
-        if user.is_user_registered():
-            self.bot.send_message(message.chat.id, 'Вы уже зарегистрированы!')
-        else:
-            self.bot.send_message(message.chat.id, 'Введите ваше имя')
-            self.bot.register_next_step_handler(message, self.name_reg)
-
-    def name_reg(self, message):
-        user_id = message.from_user.id
-        user = self.users[user_id]
-        user.name = message.text.strip()
-        self.bot.send_message(message.chat.id, 'Введите пароль')
-        self.bot.register_next_step_handler(message, self.pass_reg)
-
-    def pass_reg(self, message):
-        user_id = message.from_user.id
-        user = self.users[user_id]
-        user.password = message.text.strip()
-        user.register_user()
-        self.bot.send_message(message.chat.id, 'Пользователь успешно зарегистрирован!')
+    else:    
+        bot.send_message(message.chat.id, 'Введите ваше имя')
+        bot.register_next_step_handler(message, name_reg)
         
-    @bot.message_handler(commands=['unregister'])
-    def unregister_user(self, message):
-        user_id = message.from_user.id
-        user = self.users[user_id]
-        if user.is_user_registered():
-            self.bot.send_message(message.chat.id, 'Введите пароль и имя пользователя в формате -> Имя*Пароль')
-            self.bot.register_next_step_handler(message, self.do_unreg)
-        else:
-            self.bot.send_message(message.chat.id, 'Вы не зарегистрированы!')
+    cur.execute('SELECT * FROM reg_inf WHERE usid = ?', (message.from_user.id,))
+    search_id = cur.fetchone()
+    
+    cur.close()
+    conn.close()
+        
 
-    def do_unreg(self, message):
-        user_id = message.from_user.id
-        user = self.users[user_id]
-        uname_upass = message.text.strip().split('*')
-        if user.name == uname_upass[0] and user.password == uname_upass[-1]:
-            user.unregister_user()
-            self.bot.send_message(message.chat.id, 'Вы успешно разрегистрированы!')
-        else:
-            self.bot.send_message(message.chat.id, 'Пользователь не найден!')
+def name_reg(message):
+    global usname
 
-    def making_task(self, message):
-        user_id = message.from_user.id
-        user = self.users[user_id]
-        if user.is_user_registered():
-            if message.text == "Создать заявку":
-                self.bot.send_message(message.chat.id, "Назовите тему заявки!")
-                self.bot.register_next_step_handler(message, self.process_topic)
-        else:
-            self.bot.send_message(message.chat.id, 'Вы не зарегистрированы!')
+    usname = message.text.strip()
 
-    def process_topic(self, message):
-        topic = message.text
-        self.bot.send_message(message.chat.id, "Назовите срочность заявки (от 1 до 10)!")
-        self.bot.register_next_step_handler(message, self.process_urgency, topic)
+    bot.send_message(message.chat.id, 'Введите пароль')
 
-    def process_urgency(self, message, topic):
-        urgency = message.text
-        self.bot.send_message(message.chat.id, "Назовите глобальность проблемы!")
-        self.bot.register_next_step_handler(message, self.process_globality, topic, urgency)
+    bot.register_next_step_handler(message, pass_reg)
 
-    def process_globality(self, message, topic, urgency):
-        globality = message.text
-        # Сохранение ответов в переменных или базе данных
-        # Вывод информации о заявке
-        response = f"Проблема: {topic}\nСрочность проблемы: {urgency}\nГлобальность: {globality}"
-        self.bot.send_message(message.chat.id, response)
-        self.bot.send_message(message.chat.id, 'Ваша заявка на рассмотрении!!!')
 
-    def polling(self):
-        self.bot.polling()
+def pass_reg(message):
+    global register
+    password = message.text.strip()
 
-bot = Bot(bot_token)
+    conn = sqlite3.connect('D:\Desktop\Yandex_tasks\GLPItelegramBot\database_reg.db')
+    cur = conn.cursor()
+
+    cur.execute('INSERT INTO reg_inf(name, pass, usid) VALUES(?, ?, ?)', (usname, password, message.from_user.id))
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    register = True
+    bot.send_message(message.chat.id, 'Пользователь успешно зарегистрирован!')    
+    
+
+@bot.message_handler(commands=['unregister'])
+def start_unregister(message):
+    global flag
+    bot.send_message(message.chat.id, 'Введите пароль и имя пользователя в формате -> Имя*Пароль')
+    flag = True
+    bot.register_next_step_handler(message, do_unreg)
+
+@bot.message_handler(func=lambda mes: flag)
+def do_unreg(message):
+    global uname_upass, flag
+    uname_upass = message.text.strip().split('*')
+    conn = sqlite3.connect('D:\Desktop\Yandex_tasks\GLPItelegramBot\database_reg.db')
+    cur = conn.cursor()
+
+    cur.execute('SELECT * FROM reg_inf WHERE name = ? AND pass = ? AND usid = ?', (uname_upass[0], uname_upass[-1], message.from_user.id))
+    result = cur.fetchone()
+
+    if result:
+        cur.execute('DELETE from reg_inf WHERE name = ? AND pass = ?', (uname_upass[0], uname_upass[-1]))
+        conn.commit()
+        register = False
+        bot.send_message(message.chat.id, 'Вы успешно разрегистрированы!')
+        
+    elif not result:
+        bot.send_message(message.chat.id, 'Пользователь не найден!')
+    flag = False
+        
+    cur.execute('SELECT * FROM reg_inf WHERE usid = ?', (message.from_user.id,))
+    search_id = cur.fetchone()
+    
+    cur.close()
+    conn.close()
+    
+    
+
+@bot.message_handler(func=lambda m: True)
+def making_task(message):
+    if message.text == "Создать заявку":
+        bot.send_message(message.chat.id, "Назовите тему заявки!")
+        bot.register_next_step_handler(message, process_topic)
+    
+
+def process_topic(message):
+    topic = message.text
+    bot.send_message(message.chat.id, "Назовите срочность заявки (от 1 до 10)!")
+    bot.register_next_step_handler(message, process_urgency, topic)
+
+
+def process_urgency(message, topic):
+    urgency = message.text
+    bot.send_message(message.chat.id, "Назовите глобальность проблемы!")
+    bot.register_next_step_handler(message, process_globality, topic, urgency)
+
+
+
+def process_globality(message, topic, urgency):
+    globality = message.text
+    # Сохранение ответов в переменных или базе данных
+    # Вывод информации о заявке
+    response = f"Проблема: {topic}\nСрочность проблемы: {urgency}\nГлобальность: {globality}"
+    bot.send_message(message.chat.id, response)
+    bot.send_message(message.chat.id, 'Ваша заявка на рассмотрении!!!')
+
+
 bot.polling()
+
+# ООП пока в  пролёте
